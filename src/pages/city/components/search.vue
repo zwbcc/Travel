c397e3723b8c278bf8b8f96009757748b7438520
zwbcc <1095561636@qq.com>
2018-09-13 11:53:05 +0800

    city搜索部分

---
+++ src/pages/city/components/search.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="search">
+  <input type="text" class="search-input" placeholder="输入城市或拼音">
+</div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style scoped lang="stylus">
+  @import '~styles/varibles.styl'
+  .search
+    height .72rem
+    padding: 0 .1rem
+    background: $bgColor
+    .search-input
+      box-sizing: border-box
+      height: .62rem
+      width: 100%
+      text-align: center
+      border-radius : .06rem
+      line-height: .62rem
+      color: #666
+      padding: 0 .1rem
+</style>
+
